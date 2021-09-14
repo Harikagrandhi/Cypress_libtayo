ce972c5fabfa59de8598935f97d44c5f78bcd87c
Harikagrandhi <40540573+Harikagrandhi@users.noreply.github.com>
2021-09-14 14:31:08 -0500

    Add files via upload

---
+++ applitools.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    showLogs: true,
+    apiKey: "FH99VxPrj7abxtRxKz99ID1vxElSFuEsHOXkOOeyEmvq0110",
+    testConcurrency: true,
+    failCypressOnDiff: false,
+    disableBrowserFetching: true,
+    sendDom: false
+}
